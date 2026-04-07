3bb81c79fa87af2da16730764ba5fa602a94d993
Casey Link <unnamedrambler@gmail.com>
2026-04-07 10:05:52 +0200

    datahike-lmdb is fixed

--- datahike-lmdb-test/README.md
+++ datahike-lmdb-test/README.md
@@ -1,2 +1,4 @@
 
 ref: https://github.com/replikativ/datahike-lmdb/issues/1#issuecomment-4194256713
+
+fixed in: https://github.com/replikativ/datahike-lmdb/issues/1#issuecomment-4195236666
--- datahike-lmdb-test/readme.clj
+++ datahike-lmdb-test/readme.clj
@@ -1,25 +1,21 @@
 (ns readme)
-
 (require '[datahike.api :as d])
-(require '[datahike-lmdb.core])  ;; Registers :lmdb backend
+(require '[datahike-lmdb.core])
+(defn -main []
+  (let [cfg {:store {:backend :lmdb
+                     :id #uuid "550e8400-e29b-41d4-a716-446655440000"
+                     :path "./data"}
+             :schema-flexibility :write
+             :keep-history? false}
+        _ (when-not (d/database-exists? cfg) (d/create-database cfg))
+        conn (d/connect cfg)]
+    ;; Use datahike normally
+    (d/transact conn [{:db/ident :person/name
+                       :db/valueType :db.type/string
+                       :db/cardinality :db.cardinality/one}])
 
-(def cfg {:store {:backend :lmdb
-                  :id #uuid "550e8400-e29b-41d4-a716-446655440000"
-                  :path "./data"}
-          :schema-flexibility :write
-          :keep-history? false})
+    (d/transact conn [{:person/name "Alice"}])
 
-(d/create-database cfg)
-(def conn (d/connect cfg))
-
-;; Use datahike normally
-(d/transact conn [{:db/ident :person/name
-                   :db/valueType :db.type/string
-                   :db/cardinality :db.cardinality/one}])
-
-(d/transact conn [{:person/name "Alice"}])
-
-(prn (d/q '[:find ?n :where [?e :person/name ?n]] @conn))
-;; => #{["Alice"]}
-
-(d/release conn)
+    (prn (d/q '[:find ?n :where [?e :person/name ?n]] @conn))
+    ;; => #{["Alice"]}
+    (d/release conn)))
